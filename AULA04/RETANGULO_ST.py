342130481f109008a9a4eeffce548837361f449e
tellescps1986 <tellescps@gmail.com>
2025-11-03 21:39:05 -0300

    Aula3_2

--- AULA04/RETANGULO_ST.py
+++ AULA04/RETANGULO_ST.py
@@ -7,10 +7,14 @@ st.write("Digite o comprimento do retângulo")
 comprimento = st.number_input("Comprimento (m):")
 
 st.write("Digite o comprimento do retângulo")
-altura = st.number_input("Comprimento (m):")
+altura = st.number_input("Altura (m):")
 
 #Processamento de Dados
 area = comprimento * altura
+perimetro = 2*comprimento + 2*altura
+diagonal = (comprimento**2 + altura**2)**0.5
 
 #Saída de dados
 st.write(f"A área do terreno é de: {area}.")
+st.write(f"O Perímetro é de: {perimetro}.")
+st.write(f"O Perímetro é de: {diagonal}.")
