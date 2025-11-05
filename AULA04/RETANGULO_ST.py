6a217ae602923a26065c07709b40ce3cef513e82
tellescps1986 <tellescps@gmail.com>
2025-11-05 18:46:32 -0300

    Add virtual environment scripts and area calculation app
    - Created activate.bat and deactivate.bat scripts for virtual environment management.
    - Added pip and Python executables for the virtual environment.
    - Configured pyvenv.cfg for the virtual environment.
    - Developed MEDIDAS.py for calculating the area of a square, triangle, and trapezoid using Streamlit.
    - Added SEVAPP.py as a placeholder for future development.

--- AULA04/RETANGULO_ST.py
+++ AULA04/RETANGULO_ST.py
@@ -1,20 +1,24 @@
 import streamlit as st 
+import math as mt
 
-st.title("Problema Retângulo")
+TITULO=("Problema Retângulo")
+st.title(TITULO)
 
 # Entrada de dados
 st.write("Digite o comprimento do retângulo")
-comprimento = st.number_input("Comprimento (m):")
+comprimento = st.number_input("Comprimento (m):", min_value=0.0, format="%.2f")
 
 st.write("Digite o comprimento do retângulo")
-altura = st.number_input("Altura (m):")
+altura = st.number_input("Altura (m):", min_value=0.0, format="%.2f")
 
 #Processamento de Dados
 area = comprimento * altura
 perimetro = 2*comprimento + 2*altura
-diagonal = (comprimento**2 + altura**2)**0.5
+#diagonal = (comprimento**2 + altura**2)**0.5
+x = mt.pow(comprimento,2)+mt.pow(altura,2)
+diagonal = mt.sqrt(x)
 
 #Saída de dados
-st.write(f"A área do terreno é de: {area}.")
-st.write(f"O Perímetro é de: {perimetro}.")
-st.write(f"O Perímetro é de: {diagonal}.")
+st.write(f"A área do terreno é de: {area:.2f}.")
+st.write(f"O Perímetro é de: {perimetro:.2f}.")
+st.write(f"O Perímetro é de: {diagonal:.2f}.")
---
+++ AULA05/MEDIDAS.py
@@ -0,0 +1,30 @@
+import streamlit as st 
+import math as mt
+
+TITULO=("CÁLCULO DE ÁREA DE UM QUADRADO, TRIÂNGULO E TRAPÉZIO")
+st.markdown(f"<h1 style='text-align: center;'>{TITULO}</h1>", unsafe_allow_html=True)
+
+# Entrada de Dados
+st.write("Digite a medida A")
+medida_A = st.number_input("Medida A", min_value=0.0, format="%.2f")
+
+st.write("Digite a medida B")
+medida_B = st.number_input("Medida B", min_value=0.0, format="%.2f")
+
+st.write("Digite a medida C")
+medida_C = st.number_input("Medida C", min_value=0.0, format="%.2f")
+
+# Processamento de Dados
+area_do_quadrado = mt.pow(medida_A,2)
+
+area_do_triangulo = (medida_A * medida_B)/2
+
+area_do_trapezio = ((medida_A+medida_B)*medida_C)/2
+
+# Saída de Dados
+st.write(f"A área do quadrado é de: {area_do_quadrado:.2f}.")
+st.write(f"A área do triângulo é de: {area_do_triangulo:.2f}.")
+st.write(f"A área do trapézio é de: {area_do_trapezio:.2f}.")
+
+
+
