d7a0edcc3d84d9975391eb10c01f8704118ba9f5
tellescps1986 <tellescps@gmail.com>
2025-11-05 21:44:42 -0300

    BÁSKARA_4

--- AULA06/BASKARA_ST.py
+++ AULA06/BASKARA_ST.py
@@ -1,12 +1,18 @@
 from streamlit import header, write, text_input, button, warning, success, error
 from math import sqrt, pow
 
+#Criação de Função em Python
+def calculo(delta):
+    valor = (sqrt(delta)) / (2 * a)
+    return valor
+
+
 header('Cálculo Bháskara')
 write("Calculadora de raízes de uma equação de segundo grau")
 write("ax² + bx + c")
 
 # Entrada de dados
-a = text_input('Digite o valor de a:')
+a = text_input('Digite o valor de a:', icon='🍦')#ícone WINDOWNS + .
 b = text_input('Digite o valor de b:')
 c = text_input('Digite o valor de c:')
 
@@ -23,13 +29,15 @@ if button('Calcular raízes'):
         if delta < 0:
             warning("A equação não possui raízes reais.")
         elif delta == 0:
-            raiz = (-b + sqrt(delta)) / (2 * a)
+            raiz = (-b + calculo(delta))
             success(f"A equação possui uma raiz real: {raiz:.4f}")
         else:
-            raiz1 = (-b + sqrt(delta)) / (2 * a)
-            raiz2 = (-b - sqrt(delta)) / (2 * a)
-            success(f"As raízes da equação são:\n"
-                       f"X': {raiz1:.4f}\n"
+            raiz1 = (-b + calculo(delta))
+            raiz2 = (-b - calculo(delta))
+            success(f"As raízes da equação são:\n\n"
+                       f"X' : {raiz1:.4f}\n\n"
                        f"X'': {raiz2:.4f}")
-    except:
+    except ValueError:
         error("Por favor, insira valores válidos para a, b e c.")
+    except ZeroDivisionError:
+        error("O valor do denominador do Δ não pode ser zero")
