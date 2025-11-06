8822c66bbeeb98be605def4d7b8925b782bdf237
tellescps1986 <tellescps@gmail.com>
2025-11-05 21:11:50 -0300

    BÁSKARA_3

--- AULA06/BASKARA_ST.py
+++ AULA06/BASKARA_ST.py
@@ -1,35 +1,35 @@
-import streamlit as st
-import math as mt
+from streamlit import header, write, text_input, button, warning, success, error
+from math import sqrt, pow
 
-st.header('Cálculo Bháskara')
-st.write("Calculadora de raízes de uma equação de segundo grau")
-st.write("ax² + bx + c")
+header('Cálculo Bháskara')
+write("Calculadora de raízes de uma equação de segundo grau")
+write("ax² + bx + c")
 
 # Entrada de dados
-a = st.text_input('Digite o valor de a:')
-b = st.text_input('Digite o valor de b:')
-c = st.text_input('Digite o valor de c:')
+a = text_input('Digite o valor de a:')
+b = text_input('Digite o valor de b:')
+c = text_input('Digite o valor de c:')
 
 # Processamento de dados
-if st.button('Calcular raízes'):
+if button('Calcular raízes'):
     try:
         a = float(a)
         b = float(b)
         c = float(c)
 
-        delta = mt.pow(b, 2) - 4 * a * c
-        st.write(f"Δ = {delta:.4f}")
+        delta = pow(b, 2) - 4 * a * c
+        write(f"Δ = {delta:.4f}")
 
         if delta < 0:
-            st.warning("A equação não possui raízes reais.")
+            warning("A equação não possui raízes reais.")
         elif delta == 0:
-            raiz = (-b + mt.sqrt(delta)) / (2 * a)
-            st.success(f"A equação possui uma raiz real: {raiz:.4f}")
+            raiz = (-b + sqrt(delta)) / (2 * a)
+            success(f"A equação possui uma raiz real: {raiz:.4f}")
         else:
-            raiz1 = (-b + mt.sqrt(delta)) / (2 * a)
-            raiz2 = (-b - mt.sqrt(delta)) / (2 * a)
-            st.success(f"As raízes da equação são:\n"
+            raiz1 = (-b + sqrt(delta)) / (2 * a)
+            raiz2 = (-b - sqrt(delta)) / (2 * a)
+            success(f"As raízes da equação são:\n"
                        f"X': {raiz1:.4f}\n"
                        f"X'': {raiz2:.4f}")
     except:
-        st.error("Por favor, insira valores válidos para a, b e c.")
+        error("Por favor, insira valores válidos para a, b e c.")
