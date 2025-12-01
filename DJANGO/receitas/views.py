9e22832b1abad21b08b5b46df6f17778c8e0c561
tellescps1986 <tellescps@gmail.com>
2025-12-01 19:34:45 -0300

    Finalização Layout Página

--- DJANGO/receitas/views.py
+++ DJANGO/receitas/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "home.html", 
+    return render(request, "page/home.html", 
     context={
                 'nome':'Receitas Django'
     }) 
