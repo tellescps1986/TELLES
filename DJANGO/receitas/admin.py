7a5f4dcd199729ba7454ce8bdedefbbf21764390
tellescps1986 <tellescps@gmail.com>
2025-12-03 20:58:21 -0300

    Final

--- DJANGO/receitas/admin.py
+++ DJANGO/receitas/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Categoria, Receita
 
 # Register your models here.
+@admin.register(Receita)
+class ReceitaAdmin(admin.ModelAdmin):
+    ...
+
+class CategoriaAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Categoria, CategoriaAdmin)
